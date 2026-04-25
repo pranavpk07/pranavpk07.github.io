e15bb79446312d616c0fc9e7215fb56d3060f772
pranavpk07 <pranavdilipkamble@gmail.com>
2026-04-26 01:13:48 +0530

    Fix vite base path for user github pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/pranavpk07.github.io/', // Update this to your repo name if it's different. If deploying to username.github.io, this should be '/'
+  base: '/', 
 })
